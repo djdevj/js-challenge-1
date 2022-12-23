dcc7b369d3d635f73da8dbef990eee5b38ef4277
Devlin Jenkins <djdevj@gmail.com>
2022-12-22 23:21:43 -0800

    updating code and form

---
+++ script.js
@@ -0,0 +1,8 @@
+// password must be between 8-16 characters with 
+// password options: lower case should be automatic, options should be uppercase, numbers, spec characters
+// form must have selectable options: dropdown box for password length, checkbox for uppercase, numbers, spec characters
+// maybe have checkboxes in grid form to save space
+// letterbox for results: font (Ariel)
+// "generate" clickable button
+// no pop-ups(alerts)
+
