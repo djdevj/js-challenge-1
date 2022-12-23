637d6d1bea226ded0d35f220f6882ac6a94b4408
Devlin Jenkins <djdevj@gmail.com>
2022-12-23 02:03:02 -0800

    updated final HTML and CSS, added new buttons

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-// password must be between 8-16 characters with 
-// password options: lower case should be automatic, options should be uppercase, numbers, spec characters
+// password must be between 8-32 characters with 
+// password options: options should be uppercase, numbers, spec characters
 // form must have selectable options: dropdown box for password length, checkbox for uppercase, numbers, spec characters
 // maybe have checkboxes in grid form to save space
 // letterbox for results: font (Ariel)
 // "generate" clickable button
 // no pop-ups(alerts)
+
+// Select generate password button
+// give them a copy button
+
+
 
