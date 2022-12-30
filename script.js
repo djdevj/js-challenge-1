916e1cfdcfd4fac5b3e8e35e12f7724bfc353a47
Devlin Jenkins <djdevj@gmail.com>
2022-12-29 22:10:15 -0800

    updated JS and HTML code

--- script.js
+++ script.js
@@ -1,85 +1,100 @@
-//DOM elements
+// DOM elements
+const resultEl = document.getElementById('result');
 const lengthEl = document.getElementById('length');
-const lowercaseEl = document.getElementById('result');
+const lowercaseEl = document.getElementById('lowercase');
 const uppercaseEl = document.getElementById('uppercase');
 const numberEl = document.getElementById('number');
-const symbolsEl = document.getElementById('symbols');
-const resultEl = document.getElementById('result');
+const symbolEl = document.getElementById('symbol');
 const generateEl = document.getElementById('generate');
 const copyEl = document.getElementById('copy');
 
-
-// place fucntions in object
-const randomFunc = {
+// random function identifiers
+const randomFunction = {
     lower: getRandomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
     symbol: getRandomSymbol
-}
 
-// make the buttons work:
-// Copy button listen
-copyEl.addEventListener('click', () => {
-	const textarea = document.createElement('textarea');
-	const password = resultEl.innerText;
-	
-	if(!password) { return; }
-	
-	textarea.value = password;
-	document.body.appendChild(textarea);
-	textarea.select();
-	document.execCommand('copy');
-	textarea.remove();
-	alert('Password copied to clipboard');
-});
-// Generate button listen
+}
+// generate event listener
 generateEl.addEventListener('click', () => {
-    const Length = +lengthEl.value;
+    const length = +lengthEl.value;
     const hasLower = lowercaseEl.checked;
     const hasUpper = uppercaseEl.checked;
     const hasNumber = numberEl.checked;
-    const hasSymbol = symbolsEl.checked;
+    const hasSymbol = symbolEl.checked;
 
-    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, Length);
-});
-// Generate Pass func
-function generatePassword (lower, upper, number, symbol, length) {
-    // initialize PW variable (var)
-    // filter out unchecked
-    // loop over the length of the call gen for each type
-    // add reulting PW to the PW var then return
+    resultEl.innerText = generatePassword(
+        hasLower, 
+        hasUpper, 
+        hasNumber, 
+        hasSymbol,
+        length
+        );
 
-    let generatedPassword = '';
+    // console.log(hasLower, hasUpper, hasNumber, hasSymbol);
+})
+
+// copy to clipboard 
+copyEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if(!password) {
+        return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password Copied!');
+})
+
+// generate password function
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatePassword = '';
+
     const typesCount = lower + upper + number + symbol;
-    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+
+    // console.log('typeCount: ', typesCount);
+
+    const typesArray = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+
+    // console.log('typesArray ', typesArray);
 
     if(typesCount === 0) {
         return '';
     }
 
-    for(let i=0; i<length; i+=typesCount) {
-        typesArr.forEach(type => {
-            const funcName = Object.keys(type)[0];
-            generatedPassword += randomFunc[funcName]();
-        })
+    for(let i = 0; i < length; i += typesCount) {
+        typesArray.forEach(type => {
+            const functionName = Object.keys(type)[0];
+            // console.log('functionName: ', functionName);
+
+            generatePassword += randomFunction[functionName]();
+        });
     }
 
-    const finalPassword = generatedPassword.slice(0, length);
+    const finalPassword = generatePassword.slice(0, length);
 
     return finalPassword;
 
+    // console.log(generatePassword.slice(0, length));
 }
-// gen functions
+
+//generator functions
 function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
 function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
 }
 function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() *10) + 48);
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
 function getRandomSymbol() {
-    const symbols = '!@#$%^&*()';
-    return symbols [Math.floor(Math.random() * symbols.length)];
+    const symbol = '!@#$%^&*()';
+    return symbol[Math.floor(Math.random() * symbol.length)]
 }
